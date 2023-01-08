60578be9d0ee59e352ce21cef6c849d576399a77
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2023-01-08 18:15:19 -0500

    main.js
    Added some javascript logic

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Capturing the list elements
+let links = document.querySelectorAll(".list li");
+const linksArray = Array.from(links);
+let chosenRating;
+
+links.forEach((ele) => {
+    ele.onclick = () => {
+        links.forEach((ele) => {
+            ele.classList.remove("done");
+        });
+        ele.classList.add("done");
+    };
+});
+
+// Getting the span where it catches the rating number
+let ratingSpan = document.querySelector("rating-num");
+
+// Getting the submit button
+let submitBtn = document.querySelector("button");
+
+submitBtn.onclick = () => {
+    linksArray.forEach((ele) => {
+        if (ele.classList.contains("done")) {
+            chosenRating = ele.dataset.num;
+        }
+    });
+    console.log(chosenRating);
+};
